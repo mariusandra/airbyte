391ab7912970ca1768d98aad44a8c4d3203629ee
Vadym Hevlich <vege1wgw@gmail.com>
2021-12-22 20:18:50 +0200

    Add Detect Changed Modules and Build Reports workflow

---
+++ tools/bin/build_static_checkers_reports.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+import sys
+
+if __name__ == "__main__":
+    print(sys.argv)
---
+++ tools/bin/detect_changed_modules.py
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+import os
+import sys
+
+
+def find_base_path(dir_path: str, modules: list):
+    init_file = "__init__.py"
+    is_python_module = init_file in os.listdir(dir_path)
+    if is_python_module:
+        up_level_dir = os.path.dirname(dir_path)
+        find_base_path(up_level_dir, modules)
+    else:
+        modules.append(dir_path)
+
+
+def list_changed_modules(changed_files):
+    modules = []
+    for file_path in changed_files:
+        dir_path = os.path.dirname(file_path)
+        find_base_path(dir_path, modules)
+    return modules
+
+
+if __name__ == "__main__":
+    changed_modules = list_changed_modules(sys.argv[1:])
+    print(changed_modules)
